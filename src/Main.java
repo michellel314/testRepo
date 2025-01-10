a2612c643f3a52620e5db8f04193f69e2d790c7c
michellel314 <michelle.lee.087@gmail.com>
2025-01-10 13:42:15 -0500

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("wASSup");
         System.out.println("E");
+        System.out.println("You got some lemonade?");
     }
 }
---
+++ src/newClass.java
@@ -0,0 +1,13 @@
+public class newClass {
+    private String name;
+    private int num;
+
+    public newClass(String name, int num){
+        this.name = name;
+        this.num = num;
+    }
+
+    public int getNum(){
+        return num;
+    }
+}
