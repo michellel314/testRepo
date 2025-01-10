ca0bd4c628f8378b9a00cc13544e78bc298effc7
michellel314 <michelle.lee.087@gmail.com>
2025-01-10 13:52:10 -0500

    Fifth Commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
         System.out.println("Dear heavenly father, \n please bless us with good rng. \n in jesus' name we pray \n amen");
         System.out.println("Michelle said to the duck: what the quack are you");
         System.out.println("You got some lemonade?");
-        System.out.println("aND HE waddled away");
+        System.out.println("and the man said, no but we got some grapes");
     }
 }
