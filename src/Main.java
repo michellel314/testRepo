a9c0a554f9a418b6ab901368ebccbf27d5e17fc4
BT_1N3_23 <jintaoh123@gmail.com>
2025-01-10 13:48:43 -0500

    dear heavenly father v2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("wASSup");
-        System.out.println("There was once a duck waddling");
+        System.out.println("Dear heavenly father, \n please bless us with good rng. \n in jesus' name we pray \n amen");
         System.out.println("The duck asked the man: ");
         System.out.println("You got some lemonade?");
         System.out.println("aND HE waddled away");
